5525e62ca3213532daf9bf2f6ec6a974719daf9c
Julio Aguilar Jimenez <jcaj1n17@soton.ac.uk>
2019-03-22 12:18:59 -0600

    Initial Commit

---
+++ myChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myChat
+//
+//  Created by Julio Cesar Aguilar Jimenez on 22/03/2019.
+//  Copyright © 2019 Julio C. Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
